06e50c7cb1fd0687af50acddd990fd3214f52057
raghuttam1508 <raghu.parvatikar@gmail.com>
2022-02-16 16:48:48 +0530

    build

--- flutter_application1/lib/main.dart
+++ flutter_application1/lib/main.dart
@@ -4,8 +4,8 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-
-void main(){
-
-  runApp(MaterialApp(home: Container(color: Colors.purple),))
+void main() {
+  runApp(MaterialApp(
+    home: Container(color: Colors.purple),
+  ));
 }
